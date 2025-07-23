c2213fd0ab50faddff8e0da90b8b577704aa4d04
kaminami <ka.minami@nifty.com>
2025-07-23 23:36:02 +0900

    SPAのコンポーネントをTopとHelloに置き換え。

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {Hello} from './hello/hello/hello';
+import {Top} from './top/top/top';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Top,
+    title: 'Top'
+  },
+  {
+    path: 'hello',
+    component: Hello,
+    title: 'Hello'
+  },
+];
--- webapp/src/app/app.ts
+++ webapp/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import {routes} from './app.routes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('webapp');
+  protected readonly title = signal('Hybrid App Showcase');
+  protected readonly routes = routes;
 }
